2d31cb2a9fadc744e9bdcf152b8d6d0424aecfab
JulioApprizaPay <julio.torres@apprizapay.com>
2025-06-20 20:03:10 -0600

    parameter improvements

--- www/AlviereCaptureCheck.js
+++ www/AlviereCaptureCheck.js
@@ -5,7 +5,7 @@ exports.captureCheck = function (accountUUID, token, success, error) {
 };
 
 exports.captureDosier = function (accountUUID, docList, token,success, error) {
-    exec(success, error, 'AlviereCaptureCheck', 'captureDosier', [[docList], accountUUID, token]);
+    exec(success, error, 'AlviereCaptureCheck', 'captureDosier', [docList, accountUUID, token]);
 };
 
 exports.setEnvironment = function (environment, success, error) {
